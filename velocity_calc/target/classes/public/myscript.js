2e528921fbf14a84a063e8e0f10b4162dd9fc9a3
Gaston Sabugo <gastons_92@hotmail.com>
2020-11-11 13:18:00 -0300

    optimizacion de codigo js

--- velocity_calc/target/classes/public/myscript.js
+++ velocity_calc/target/classes/public/myscript.js
@@ -1,15 +1,15 @@
 //var no_operator = ("none" === document.calculator.action.substr(-4));
     var first_num = true;
     function borrar(){
-        $('input[name=a]').val('0');
-        $('input[name=b]').val('0');
         /*
         document.calculator.a.value = 0;
         document.calculator.b.value = 0;
         document.calculator.action = "/none";
          */
-        first_num = true;
+        $('input[name=a]').val('0');
+        $('input[name=b]').val('0');
         $('#calc').attr('action', '/none');
+        first_num = true;
     }
 
     function num0(){
@@ -20,86 +20,14 @@
         if(document.calculator.b.value != 0)
             document.calculator.b.value+='0';
     }
-
-    function num1(){
+    
+    function num(numero){
         if(document.calculator.b.value == 0 || first_num){
-            document.calculator.b.value = 1;
+            document.calculator.b.value = numero;
             first_num = false;
         }
         else
-            document.calculator.b.value+='1';
-    }
-
-    function num2(){
-        if(document.calculator.b.value == 0 || first_num){
-            document.calculator.b.value = 2;
-            first_num = false;
-        }
-        else
-            document.calculator.b.value+='2';
-    }
-
-    function num3(){
-        if(document.calculator.b.value == 0 || first_num){
-            document.calculator.b.value = 3;
-            first_num = false;
-        }
-        else
-            document.calculator.b.value+='3';
-    }
-
-    function num4(){
-        if(document.calculator.b.value == 0 || first_num){
-            document.calculator.b.value = 4;
-            first_num = false;
-        }
-        else
-            document.calculator.b.value+='4';
-    }
-                
-    function num5(){
-        if(document.calculator.b.value == 0 || first_num){
-            document.calculator.b.value = 5;
-            first_num = false;
-        }
-        else
-            document.calculator.b.value+='5';
-    }
-
-    function num6(){
-        if(document.calculator.b.value == 0 || first_num){
-            document.calculator.b.value = 6;
-            first_num = false;
-        }
-        else
-            document.calculator.b.value+='6';
-    }
-		
-    function num7(){
-        if(document.calculator.b.value == 0 || first_num){
-            document.calculator.b.value = 7;
-            first_num = false;
-        }
-        else
-            document.calculator.b.value+='7';
-    }
-
-    function num8(){
-        if(document.calculator.b.value == 0 || first_num){
-            document.calculator.b.value = 8;
-            first_num = false;
-        }
-        else
-            document.calculator.b.value+='8';
-    }
-
-    function num9(){
-        if(document.calculator.b.value == 0 || first_num){
-            document.calculator.b.value = 9;
-            first_num = false;
-        }
-        else
-            document.calculator.b.value+='9';
+            document.calculator.b.value+= numero.toString();
     }
         
     function suma() {
